5bdff669da9229dc1257a79a225ceb08eeda7bf1
hotaka-mt <hhhotaka2004@gmail.com>
2024-06-24 02:12:37 +0900

    ゲイン設定画面制作

--- build/DebugPkg/build/lib/DebugPkg/debug_node.py
+++ build/DebugPkg/build/lib/DebugPkg/debug_node.py
@@ -1,64 +1,95 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import UInt8MultiArray
 from std_msgs.msg import Int16MultiArray
 import tkinter as tk
 
 class DebugNode(Node):
     def __init__(self,window_name="DebugWindow",geometory="1024x600"):
         super().__init__("debug_node")
+        self.serial_send = Float32MultiArray()
+        for i in range(6):
+            self.serial_send.data.append(0)
 
         self.rs_data = [0]*3
-        self.serial = Float32MultiArray()
-        for i in range(5):
-            self.serial.data.append(0)
+        self.serial_rece_data = [0]*38
         self.im920_data = [0]*3
         self.pos_data = [0]*4
 
         self.rs_sub = self.create_subscription(Float32MultiArray,"RS_topic",self.get_realsense,10)
-        self.serial_sub = self.create_subscription(Float32MultiArray,"reception_topic",self.get_serial,10)
+        self.serial_sub = self.create_subscription(UInt8MultiArray,"reception_topic",self.get_serial,10)
         self.IM920_sub = self.create_subscription(Int16MultiArray,"IM920_rece_topic",self.get_IM920,10)
         self.pos_sub = self.create_subscription(Float32MultiArray,"pos_topic",self.get_position,10)
 
         self.serial_pub = self.create_publisher(Float32MultiArray,"send_topic",10)
         self.IM920_pub = self.create_publisher(Float32MultiArray,"IM920_send_topic",10)
+
+        self.x_vel = 0
+        self.y_vel = 0
+        self.omega = 0
         
         #ウィンドウ設定
         self.root = tk.Tk()
         self.root.title(window_name)
         self.root.geometry(geometory)
         #リセットボタンを作成
-        self.reset_button = tk.Button(text = "ホームに戻る",font = ("MSゴシック","20"))
-        self.reset_button.bind('<Button-1>',self.GUI_reset)
-        self.reset_button.place(x=830,y=10)
-
-        self.x_vel = 0
-        self.y_vel = 0
-        self.omega = 0
-
+        self.home_button = tk.Button(text = "ホームに戻る",font = ("MSゴシック","20"))
+        self.home_button.bind('<Button-1>',self.GUI_reset)
+        self.home_button.place(x=830,y=10)
+        #設定ボタン作成
+        self.setting_button = tk.Button(text="設定",font=("メイリオ","20"),command=self.set_param)
+        self.setting_button.place(x=20,y=10)
         #各ボタンの作成と設置
         self.main_menu()
         self.root.mainloop()
 
-
     def main_destroy(self):
         try: self.manual_botton.destroy() 
+        except: pass
+        try: self.FB_scale.destroy()
+        except: pass
+        try: self.RL_scale.destroy()
+        except: pass
+        try: self.Omega_scale.destroy()
+        except: pass
+        try: self.reset_button.destroy()
+        except: pass
+        for i in range(15):
+            try: self.param_entry[i].destroy()
+            except: pass
+            try: self.param_label[i].destroy()
+            except: pass
+            try: self.param_button[i].destroy()
+            except: pass
+        try: self.set_param_button.destroy()
         except: pass
 
     def GUI_reset(self,event):
         self.main_destroy()
         self.main_menu()
 
-    def control(self):  #手動操作用
-        print("control")
+    def M1_update(self): self.param_publish(0)
+    def M2_update(self): self.param_publish(1)
+    def M3_update(self): self.param_publish(2)
+    def M4_update(self): self.param_publish(3)
+    def other_update(self): self.param_publish(4)
+
+    def param_update(self):
+        with open("src/DebugPkg/DebugPkg/robot_param.md",mode="w") as param:
+            for i in range(15):
+                if self.param_entry[i].get() != "":
+                    param.write(self.Param[i*2]+"   "+self.param_entry[i].get()+"\n")
+                else:
+                    param.write(self.Param[i*2]+"   "+self.Param[i*2+1]+"\n")
+        self.set_param()
     
     def update_velo(self,event):
         self.x_vel = self.RL_scale_var.get()
         self.y_vel = self.FB_scale_var.get()
         self.omega = self.Omega_scale_var.get()
         print(f"{self.x_vel}    {self.y_vel}    {self.omega}")
-        self.serialpublish(self.x_vel,self.y_vel,self.omega)
+        self.mecanum_publish(self.x_vel,self.y_vel,self.omega)
 
     def reset_velo(self):
         self.x_vel = 0
@@ -68,16 +99,37 @@ class DebugNode(Node):
         self.RL_scale_var.set(0)
         self.Omega_scale_var.set(0)
         print(f"{self.x_vel}    {self.y_vel}    {self.omega}")
-        self.serialpublish(self.x_vel,self.y_vel,self.omega)
+        self.mecanum_publish(self.x_vel,self.y_vel,self.omega)
 
     def main_menu(self):
         self.main_destroy()
         #手動操作
         self.manual_botton = tk.Button(text="手動操作",font=("メイリオ","20"),command=self.manual_control)
         self.manual_botton.place(x=20,y=60,width=450,height=200)
-        #設定用
-        self.setting_button = tk.Button(text="設定",font=("メイリオ","20"),command=self.control)
-        self.setting_button.place(x=20,y=10)
+    
+    def set_param(self):
+        self.main_destroy()
+        self.param_entry = [0]*15
+        self.param_label = [0]*15
+        self.param_button = [0]*5
+        with open("src/DebugPkg/DebugPkg/robot_param.md") as param:
+            self.Param = param.read().split()
+            for i in range(15):
+                self.param_entry[i] = tk.Entry(font=("メイリオ","20"),width=5)
+                self.param_label[i] = tk.Label(text=self.Param[i*2]+"    "+self.Param[i*2+1],font=("メイリオ","20"))
+                
+                self.param_entry[i].place(x=(i//9)*560+250,y=(i%9)*55+60)
+                self.param_label[i].place(x=(i//9)*500+ 10,y=(i%9)*55+60)
+    
+        self.param_button[0] = tk.Button(text="反映",font=("メイリオ","20"),command=self.M1_update)
+        self.param_button[1] = tk.Button(text="反映",font=("メイリオ","20"),command=self.M2_update)
+        self.param_button[2] = tk.Button(text="反映",font=("メイリオ","20"),command=self.M3_update)
+        self.param_button[3] = tk.Button(text="反映",font=("メイリオ","20"),command=self.M4_update)
+        self.param_button[4] = tk.Button(text="反映",font=("メイリオ","20"),command=self.other_update)
+        self.set_param_button = tk.Button(text="更新",font=("メイリオ","20"),command=self.param_update)
+        for i in range(5):
+            self.param_button[i].place(x=(i//3)*560+350,y=(i%3)*165+170,width=80,height=40)
+        self.set_param_button.place(x=800,y=450,height=80,width=80)
 
     def manual_control(self):
         self.main_destroy()
@@ -93,24 +145,38 @@ class DebugNode(Node):
         self.RL_scale.place(x=300,y=270)
         self.Omega_scale.place(x=400,y=400)
 
-    def serialpublish(self,x,y,omega):
-        self.serial.data[0] = float(x)
-        self.serial.data[1] = float(y)
-        self.serial.data[2] = float(omega)
-        self.serial.data[3] = 0
-        self.serial.data[4] = 0
-        self.serial_pub.publish(self.serial)
+    def mecanum_publish(self,x,y,omega):
+        self.serial_send.data[0] = 0
+        self.serial_send.data[1] = float(x)
+        self.serial_send.data[2] = float(y)
+        self.serial_send.data[3] = float(omega)
+        self.serial_send.data[4] = 0
+        self.serial_send.data[5] = 0
+        self.serial_pub.publish(self.serial_send)
+
+    def param_publish(self,num):
+        self.serial_send.data[0] = float(num+1)
+        self.serial_send.data[1] = float(self.Param[(num*3)*2+1])
+        self.serial_send.data[2] = float(self.Param[(num*3)*2+1+2])
+        self.serial_send.data[3] = float(self.Param[(num*3)*2+1+4])
+        self.serial_send.data[4] = 0
+        self.serial_send.data[5] = 0
+        self.serial_pub.publish(self.serial_send)
 
     def get_realsense(self,msg):
+        #print(msg.data)
         for i in range(3):
             self.rs_data[i] = msg.data[i]
     def get_serial(self,msg):
-        for i in range(4):
-            self.serial_data[i] = msg.data[i]
+        #print(msg.data)
+        for i in range(38):
+            self.serial_rece_data[i] = msg.data[i]
     def get_IM920(self,msg):
+        #print(msg.data)
         for i in range(3):
             self.im920_data[i] = msg.data[i]
     def get_position(self,msg):
+        #print(msg.data)
         for i in range(3):
             self.pos_data[i] = msg.data[i]
 
@@ -118,7 +184,6 @@ def main(args = None):
     rclpy.init(args=args)
 
     debug_node = DebugNode()
-
     rclpy.spin(debug_node)
     debug_node.destroy_node()
     rclpy.shutdown()
--- install/SerialPkg/lib/python3.10/site-packages/SerialPkg/serial_node.py
+++ install/SerialPkg/lib/python3.10/site-packages/SerialPkg/serial_node.py
@@ -1,62 +1,56 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import UInt8MultiArray
 from SerialPkg import my_serial as serial
 
 class SerialNode(Node):
     def __init__(self):
         super().__init__('serial_node')
         self.serial = serial.MySerial('/dev/ttyUSB0',115200)
+        
         self.tx_data = [0]*15   #ROS->STM
+        self.tx_select = [0xA0,0xA1,0xA2,0xA3,0xA4,0xA5]
+
+        self.rx_bytes = [0]*39   #ROS<-STM
         self.rx_data = [0]*39   #ROS<-STM
         
         self.x_pos = 0
         self.y_pos = 0
         self.degree = 0
         self.condtion = 0
-        self.publish_msg = Float32MultiArray()
-        for i in range(3):
+        self.publish_msg = UInt8MultiArray()
+        for i in range(38):
             self.publish_msg.data.append(0)
 
-        self.publisher = self.create_publisher(Float32MultiArray,"reception_topic",10)
+        self.publisher = self.create_publisher(UInt8MultiArray,"reception_topic",10)
         self.subscriber = self.create_subscription(Float32MultiArray,"send_topic",self.make_send,10)
         self.timer = self.create_timer(0.01,self.interrupt)
     
     def interrupt(self):
         #ROSから送信
         self.serial.write(bytes(self.tx_data))
-        print(self.tx_data)
+        #print(self.tx_data)
         
         #マイコンから受信しトピックに出力
-        '''
-        self.rx_data = self.serial.read([0xA5],14)
+        self.rx_bytes = self.serial.read([0xA5],40)
+        for i in range(39):
+            self.rx_data[i] = self.rx_bytes[i]
+        if self.rx_data[38] != sum(self.rx_data[0:38]): return False
 
-        sum = 0
-        for i in range(13):
-            sum += self.rx_data[i]
-        print(sum)
-        if sum&0xFF != self.rx_data[13]: return False
-
-        self.x_pos  = serial.from_binary(self.rx_data[0:4],int)
-        self.y_pos  = serial.from_binary(self.rx_data[4:8],int)
-        self.degree = serial.from_binary(self.rx_data[8:12],int)
-        self.condtion = self.rx_data[12]
-        self.publish_msg.data = [self.x_pos,self.y_pos,self.degree,self.condtion]
-        print("publish serial_node")
+        for i in range(38):
+            self.publish_msg.data[i] = self.rx_data[i]
+        
         self.publisher.publish(self.publish_msg)
-        '''
-
+        print(self.rx_data)
+        
     def make_send(self,msg):
-        #for i in range(6):
-        #    print(f"{msg.data[i]}",end="    ")
-        #print()
-        self.tx_data[0]  = 0xA0
-        self.tx_data[1:5]  = serial.to_binary(msg.data[0])
-        self.tx_data[5:9]  = serial.to_binary(msg.data[1])
-        self.tx_data[9:13] = serial.to_binary(msg.data[2])
-        self.tx_data[13] = int(msg.data[3])
-        self.tx_data[14] = int(msg.data[4])
+        self.tx_data[0]  = self.tx_select[int(msg.data[0])]
+        self.tx_data[1:5]  = serial.to_binary(msg.data[1])
+        self.tx_data[5:9]  = serial.to_binary(msg.data[2])
+        self.tx_data[9:13] = serial.to_binary(msg.data[3])
+        self.tx_data[13] = int(msg.data[4])
+        self.tx_data[14] = int(msg.data[5])
 
 def main(args = None):
     rclpy.init(args=args)
